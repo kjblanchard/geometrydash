7e38890fcd8b4d6f1dc88119642daee7b4385c08
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-01-21 14:54:51 -0500

    Automated the library and music files to the build dir

--- sgengine/initialize_audio.cpp
+++ sgengine/initialize_audio.cpp
@@ -11,9 +11,9 @@ FMOD::Studio::System* destroy()
     coreSystem->setSoftwareFormat(0, FMOD_SPEAKERMODE_STEREO, 0);
     system->initialize(1024, FMOD_INIT_NORMAL, FMOD_INIT_NORMAL, nullptr);
     FMOD::Studio::Bank* mainBank = nullptr;
-    result = system->loadBankFile("../../sgengine/sfx/Master.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
+    result = system->loadBankFile("sfx/Master.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
     FMOD::Studio::Bank* stringsBank = nullptr;
-    result = system->loadBankFile("../../sgengine/sfx/Master.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
+    result = system->loadBankFile("sfx/Master.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
     FMOD::Studio::EventDescription* loadedEventDescription = nullptr;
     result = system->getEvent("event:/main", &loadedEventDescription);
     FMOD::Studio::EventInstance* loadedEventInstance = nullptr;
